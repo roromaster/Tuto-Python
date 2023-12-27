c76e22eca145d8d3647ffdf1b14e7f09589bcd4f
Roromaster <roromaster@gmail.com>
2023-12-27 16:19:50 +0100

    Initial commit

--- First Game.py
+++ First Game.py
@@ -7,6 +7,13 @@ running = True
 
 image = pygame.image.load("ball.png").convert()
 
+# Init the location of our Ball
+x = 0
+y = 0
+
+#Init clock
+clock = pygame.time.Clock()
+
 while running:
 
      for event in pygame.event.get():
@@ -15,14 +22,20 @@ while running:
      pressed = pygame.key.get_pressed()
      if pressed[pygame.K_LEFT]:
           print("Left")
+          x -= 1
      if pressed[pygame.K_RIGHT]:
           print("Right")
+          x += 1
      if pressed[pygame.K_UP]:
-          print("UO")
+          print("Up")
+          y-=1
      if pressed[pygame.K_DOWN]:
           print("Down")
-     screen.blit(image,(0,0))
+          y+=1
+     screen.fill((0,0,0))
+     screen.blit(image,(x,y))
      pygame.display.flip() 
+     clock.tick(60)
 
 
 pygame.quit()
